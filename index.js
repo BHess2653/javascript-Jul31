59f6d426ed470e20b5951c41729f0ea63451ad19
BHess2653 <GM_Dahaka@BHMac.local>
2017-07-31 17:43:03 -0400

    variables commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const i = 0;
+
+for ( i < array.length; i++) {
+  array[i]
+}
+
+
+// var is leaky
+// can overwite
+// function scope confusion.
+
+// let
+// block scope
+// can overwite it but can't reassign
+
+// const
+// block scope
+// can't overwite the variable
+// *** always start with const ***
